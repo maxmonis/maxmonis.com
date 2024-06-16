5e4a2b8b615fa31a80655ed70d52c53d4d23ffd2
Max Monis <mmonis77@gmail.com>
2024-06-16 10:15:53 -0400

    Add .prettierrc, format js

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,31 +1,31 @@
-var theme = localStorage.getItem("maxmonis.com_theme")
+var theme = localStorage.getItem("maxmonis.com_theme");
 if (theme !== "dark" && theme !== "light") {
-  theme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light"
+	theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+		? "dark"
+		: "light";
 }
 
-createToggle()
-applyTheme()
+createToggle();
+applyTheme();
 
 function applyTheme() {
-  if (theme === "dark") {
-    document.body.classList.add("dark")
-    document.querySelector(".dark-mode-toggle").innerText = "🌛"
-  } else {
-    document.body.classList.remove("dark")
-    document.querySelector(".dark-mode-toggle").innerText = "🌞"
-  }
+	if (theme === "dark") {
+		document.body.classList.add("dark");
+		document.querySelector(".dark-mode-toggle").innerText = "🌛";
+	} else {
+		document.body.classList.remove("dark");
+		document.querySelector(".dark-mode-toggle").innerText = "🌞";
+	}
 }
 
 function createToggle() {
-  var toggle = document.createElement("button")
-  toggle.classList.add("dark-mode-toggle")
-  toggle.setAttribute("aria-label", "toggle dark mode")
-  toggle.addEventListener("click", function () {
-    theme = theme === "light" ? "dark" : "light"
-    localStorage.setItem("maxmonis.com_theme", theme)
-    applyTheme()
-  })
-  document.querySelector(".page-footer").appendChild(toggle)
+	var toggle = document.createElement("button");
+	toggle.classList.add("dark-mode-toggle");
+	toggle.setAttribute("aria-label", "toggle dark mode");
+	toggle.addEventListener("click", function () {
+		theme = theme === "light" ? "dark" : "light";
+		localStorage.setItem("maxmonis.com_theme", theme);
+		applyTheme();
+	});
+	document.querySelector(".page-footer").appendChild(toggle);
 }
